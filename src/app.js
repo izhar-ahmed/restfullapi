2e4ac09dd1e70ba952ab26025b5cd44f927a6ad0
izharahmed <izharahmed1280@gmail.com>
2021-05-05 12:13:51 +0530

    adding delete api for deleting student by its id

--- src/app.js
+++ src/app.js
@@ -10,41 +10,41 @@ const app = express();
 app.use(express.json());
 
 // Create a new students
-app.post("/students",async(req, res)=>{
+app.post("/students", async (req, res) => {
 
-   
+
     try {
         const user = new Student(req.body);
         const createStudents = await user.save();
         res.status(201).send(createStudents);
     }
-    catch(e){
+    catch (e) {
         res.status(400).send(e);
     }
- 
+
 });
 
 // read all registerd students
 
-app.get("/students",async(req, res)=>{
+app.get("/students", async (req, res) => {
     try {
-    const studentsData = await Student.find();
-     res.send(studentsData);
-    }catch(e){
+        const studentsData = await Student.find();
+        res.send(studentsData);
+    } catch (e) {
         res.send(e);
     }
 })
 
 //read a perticular student data by id
 
-app.get("/students/:id",async(req, res)=>{
-    try{
+app.get("/students/:id", async (req, res) => {
+    try {
         const _id = req.params.id;
         const studentData = await Student.findById(_id);
         res.send(studentData);
 
-    }catch(e){
-           res.send(e);
+    } catch (e) {
+        res.send(e);
     }
 })
 
@@ -54,17 +54,17 @@ app.get("/students/:id",async(req, res)=>{
 //     try{
 
 //         const name = req.params.name;
-        // const studentName = await Student.find({name: name});
-        // res.send(studentName);
-        // console.log(name);
-        // if(!studentName){
-        //     return res.status(404).send();
-        // }else{
-        //     res.send(studentName);
+// const studentName = await Student.find({name: name});
+// res.send(studentName);
+// console.log(name);
+// if(!studentName){
+//     return res.status(404).send();
+// }else{
+//     res.send(studentName);
 
-        // }
+// }
 
-  
+
 //     }catch(e){
 //            res.status(500).send(e);
 //     }
@@ -72,19 +72,32 @@ app.get("/students/:id",async(req, res)=>{
 
 //Update students by its Id
 
-app.patch("/students/:id", async(req, res)=> {
-    try{
-    const _id = req.params.id;
-    const updateStudent = await Student.findByIdAndUpdate(_id, req.body,{
-        new:true
-    });
-    res.send(updateStudent);
-}catch(e){
-    res.send(e);
-}
+app.patch("/students/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const updateStudent = await Student.findByIdAndUpdate(_id, req.body, {
+            new: true
+        });
+        res.send(updateStudent);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+})
+
+//delete student by its Id
+
+app.delete("/students/:id", async (req, res) => {
+    try {
+
+        const deleteStudent = await Student.findByIdAndDelete(req.params.id);
+        res.send("delete successfuly");
+
+    } catch (e) {
+        res.status(500).send(e);
+    }
 })
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`connection is setup at ${port}`);
 });
